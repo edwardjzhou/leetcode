b4a4180702ab6744ff74fc389cdcc1a5f50f08a9
featurerich1 <52511748+featurerich1@users.noreply.github.com>
2019-08-28 09:11:44 -0500

    Add files via upload

--- duplicate zeros.rb
+++ duplicate zeros.rb
@@ -1,25 +1,39 @@
-
-
-#slow tbd: redo it smartly
+#dont know if this counts as modifying array in place -- probably not
 def duplicate_zeros(arr)
-    i=0
-
-    while i < arr.length-1
-    	if arr[i]==0
-    		n=1
-    		while n < arr.length-i-1
-    			arr[arr.length-n]=arr[arr.length-1-n]
-    			n+=1
-    		end
-
-    		arr[i+1]=0
-    		i+=1
-    	end
-    	i+=1
-
-    end
-  	return arr
+	i=0
+	while i<arr.length-1
+		if arr[i]==0
+			arr.insert(i,0)
+			arr.pop
+			i+=1
+		end
+	i+=1
+	end
+	return arr
 end
+
+
+
+# #slow tbd: redo it smartly
+# def duplicate_zeros(arr)
+#     i=0
+
+#     while i < arr.length-1
+#     	if arr[i]==0
+#     		n=1
+#     		while n < arr.length-i-1
+#     			arr[arr.length-n]=arr[arr.length-1-n]
+#     			n+=1
+#     		end
+
+#     		arr[i+1]=0
+#     		i+=1
+#     	end
+#     	i+=1
+
+#     end
+#   	return arr
+# end
 
 p duplicate_zeros([0,1,2,0,4,5,6])
 
