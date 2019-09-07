fd0d8e4a75a1412d50c0e9d22dfcc959eef79572
featurerich1 <52511748+featurerich1@users.noreply.github.com>
2019-09-07 09:53:44 -0500

    Update power of three.rb

--- power of three.rb
+++ power of three.rb
@@ -1,14 +1,14 @@
-# def is_power_of_three(n)
-#     while n > 3
-#         n = n.to_f/3.0
-#     end
+ def is_power_of_three(n)
+     while n > 3
+         n = n.to_f/3.0
+     end
     
-#     if n==3 or n==1
-#         return true
-#     else
-#         return false
-#     end
-# end
+     if n==3 or n==1
+         return true
+     else
+         return false
+     end
+ end
 
 
 
